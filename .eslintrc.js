d412465726da66d3249b0f4d9f36a6ed7891a7ca
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-06-01 14:49:34 +0200

    allow switch statements without default

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,20 @@ module.exports = {
     jest: true,
     browser: true,
   },
-  extends: ['airbnb-typescript', 'prettier', 'prettier/react', 'prettier/@typescript-eslint'],
-  parser: "@typescript-eslint/parser",
+  extends: [
+    'airbnb-typescript',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    "import/prefer-default-export": "off",
-    "import/no-unresolved": "off",
-    "prettier/prettier": "error",
-    "react/prop-types": "off",
-    "no-plusplus": "off"
-  }
+    'default-case': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    'no-plusplus': 'off',
+  },
 };
