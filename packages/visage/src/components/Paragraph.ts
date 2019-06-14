91458a2142a87d91a2732cb091590600ab42d51b
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-06-14 15:06:43 +0200

    add Paragraph component, closes #5

---
+++ packages/visage/src/components/Paragraph.ts
@@ -0,0 +1,10 @@
+import { createComponent } from '../core';
+
+export const Paragraph = createComponent('p', {
+  displayName: 'Paragraph',
+  defaultStyles: {
+    fontSize: 0,
+    lineHeight: 0,
+    my: 1,
+  },
+});
--- packages/visage/src/components/index.ts
+++ packages/visage/src/components/index.ts
@@ -3,5 +3,6 @@ export * from './Button';
 export * from './Flex';
 export * from './Grid';
 export * from './Heading';
+export * from './Paragraph';
 export * from './Svg';
 export * from './Text';
