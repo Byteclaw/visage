5baf8f949653029cea83c1322cea6474c19dd817
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-06-22 13:52:25 +0200

    add default margins and paddings for typographich elements

--- packages/visage/src/components/Text.ts
+++ packages/visage/src/components/Text.ts
@@ -3,7 +3,10 @@ import { createComponent } from '../core';
 export const Text = createComponent('span', {
   displayName: 'Text',
   defaultStyles: {
+    display: 'inline',
     fontFamily: 'body',
+    p: 0,
+    m: 0,
     mb: 0,
   },
 });
