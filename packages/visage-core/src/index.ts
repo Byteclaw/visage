182a149bd6a29458b930b989532b5bc6da2c9b45
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-07-25 10:53:56 +0200

    remove unnecessary variant creators from core

--- packages/visage-core/src/index.ts
+++ packages/visage-core/src/index.ts
@@ -1,7 +1,5 @@
-export * from './createBooleanVariant';
 export * from './createComponent';
 export * from './createTheme';
-export * from './createVariant';
 export * from './DesignSystem';
 export * from './hooks';
 export * from './types';
