87ba8b2b0c7969585a5d0682c9bf66b3a80b0c13
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-07-25 15:12:34 +0200

    fix gatsby mdx plugin settings

--- packages/visage-docs/gatsby-config.js
+++ packages/visage-docs/gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-mdx',
+      resolve: 'gatsby-plugin-mdx',
       options: {
         gatsbyRemarkPlugins: [
           { resolve: 'gatsby-remark-emoji' },
