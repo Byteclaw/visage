0eea90f1aabc607bcb0e331f088b60dc9015fe7d
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-08-11 15:06:24 +0200

    add CloseIcon svg

---
+++ packages/visage/src/assets/CloseIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// converted in https://www.smooth-code.com/open-source/svgr/playground/
+
+export const CloseIcon = (props: JSX.IntrinsicElements['svg']) => (
+  <svg viewBox="0 0 60.963 60.842" {...props}>
+    <path d="M59.595 52.861L37.094 30.359 59.473 7.98a4.676 4.676 0 00-6.611-6.611L30.483 23.748 8.105 1.369A4.676 4.676 0 001.494 7.98l22.378 22.379L1.369 52.861a4.674 4.674 0 003.306 7.98 4.66 4.66 0 003.306-1.369L30.483 36.97l22.501 22.502c.913.913 2.109 1.369 3.306 1.369s2.393-.456 3.306-1.369a4.677 4.677 0 00-.001-6.611z" />
+  </svg>
+);
