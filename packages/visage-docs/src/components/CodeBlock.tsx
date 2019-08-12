834a050a7bbc5733778aed39168ae107e6e67a94
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-08-12 11:36:17 +0200

    consolidate color themes for editors and hide code by default

--- packages/visage-docs/src/components/CodeBlock.tsx
+++ packages/visage-docs/src/components/CodeBlock.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-array-index-key */
 import Highlight, { defaultProps, Language } from 'prism-react-renderer';
-import React, { Fragment } from 'react';
+import duotoneLight from 'prism-react-renderer/themes/duotoneLight';
+import React, { Fragment, useState } from 'react';
 import { LiveEditor, LiveError, LivePreview, LiveProvider } from 'react-live';
 import * as DSScope from '@byteclaw/visage';
 import * as Core from '@byteclaw/visage-core';
@@ -9,7 +10,7 @@ import { theme } from '../theme';
 import { WithRef } from './WithRef';
 import { WithState } from './WithState';
 
-const { Box, Flex, createComponent } = DSScope;
+const { Box, Flex, Button, createComponent } = DSScope;
 const Scope = {
   Fragment,
   ...Core,
@@ -33,54 +34,75 @@ interface CodeBlockProps {
   className: string;
   children: string;
   live?: boolean;
+  expanded?: boolean;
   stringify?: boolean;
+  transpile?: boolean | 'false' | 'true';
 }
 
 export function CodeBlock({
   children,
   className: baseClassName,
   live,
+  expanded,
   stringify,
+  transpile = true,
 }: CodeBlockProps) {
+  const [isExpanded, setExpanded] = useState(expanded);
   const language: Language = baseClassName.replace(/language-/, '') as Language;
-
-  if (live) {
-    return (
-      <Flex styles={{ mb: 2 }}>
-        <LiveProvider
-          code={children.trim()}
-          transformCode={stringify ? code => `'' + ${code}` : undefined}
-          scope={Scope}
-        >
-          <Box styles={{ width: ['100%', '50%', '40%'] }}>
-            <LivePreview />
-          </Box>
-          <Box
-            styles={{ backgroundColor: 'black', width: ['100%', '50%', '60%'] }}
-          >
-            <LiveEditor />
-            <EditorError />
-          </Box>
-        </LiveProvider>
-      </Flex>
-    );
-  }
+  const shouldTranspile = transpile !== 'false' && transpile !== false;
 
   return (
-    <Box styles={{ py: 2 }}>
-      <Highlight {...defaultProps} code={children.trim()} language={language}>
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={{ ...style, padding: '20px' }}>
-            {tokens.map((line, i) => (
-              <div key={i} {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
+    <Box styles={{ mb: 2 }}>
+      {shouldTranspile ? (
+        <LiveProvider
+          code={children.trim()}
+          disabled={!live}
+          language={language}
+          transformCode={stringify ? code => `'' + ${code}` : undefined}
+          scope={Scope}
+          theme={duotoneLight}
+        >
+          <Box styles={{ mb: 2, width: '100%' }}>
+            <LivePreview />
+          </Box>
+          <Flex styles={{ width: '100%' }}>
+            <Button onClick={() => setExpanded(!isExpanded)} type="button">
+              {live
+                ? !isExpanded
+                  ? 'Show editor'
+                  : 'Hide editor'
+                : !isExpanded
+                ? 'Show code'
+                : 'Hide code'}
+            </Button>
+          </Flex>
+          {isExpanded || !live ? (
+            <Box styles={{ backgroundColor: 'black', width: '100%' }}>
+              <LiveEditor />
+              <EditorError />
+            </Box>
+          ) : null}
+        </LiveProvider>
+      ) : (
+        <Highlight
+          {...defaultProps}
+          code={children.trim()}
+          language={language}
+          theme={duotoneLight}
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={className} style={{ ...style, padding: '10px' }}>
+              {tokens.map((line, i) => (
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
+      )}
     </Box>
   );
 }
