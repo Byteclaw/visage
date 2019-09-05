e31e599501dfc5ecb1a6f599630710d4515a0137
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-09-05 13:35:23 +0200

    add align to Badge

--- packages/visage/src/components/Badge.ts
+++ packages/visage/src/components/Badge.ts
@@ -1,19 +1,31 @@
 import { createComponent, createVariant } from '../core';
 
 export const Badge = createVariant(
-  createComponent('span', {
-    displayName: 'Badge',
-    defaultStyles: {
-      borderColor: 'white',
-      borderRadius: 6,
-      borderStyle: 'solid',
-      borderWidth: 2,
-      display: 'inline-flex',
-      fontSize: -1,
-      lineHeight: -1,
-      px: 1,
+  createVariant(
+    createComponent('span', {
+      displayName: 'Badge',
+      defaultStyles: {
+        borderColor: 'white',
+        borderRadius: 6,
+        borderStyle: 'solid',
+        borderWidth: 2,
+        display: 'inline-flex',
+        fontSize: -1,
+        lineHeight: -1,
+        px: 1,
+      },
+    }),
+    'align',
+    {
+      left: {
+        marginRight: 'auto',
+      },
+      right: {
+        marginLeft: 'auto',
+      },
+      default: {},
     },
-  }),
+  ),
   'variant',
   {
     danger: {
