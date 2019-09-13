760dd2017ebdb7000b198003a8d03bd9c3662e26
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-09-13 13:47:17 +0200

    add BoxSkeleton

--- packages/visage/src/components/Box.ts
+++ packages/visage/src/components/Box.ts
@@ -1,5 +1,32 @@
+import { createElement } from 'react';
 import { createComponent } from '../core';
+import { skeletonAnimation } from './animations';
 
 export const Box = createComponent('div', {
   displayName: 'Box',
 });
+
+export const BoxSkeleton = createComponent(
+  ({
+    aspectRatio = 1,
+    ...restProps
+  }: JSX.IntrinsicElements['div'] & { aspectRatio?: number }) => {
+    const height = 100 / aspectRatio;
+    return createElement(
+      'div',
+      restProps,
+      createElement('div', {
+        style: { paddingBottom: `${height}%` },
+      }),
+    );
+  },
+  {
+    displayName: 'BoxSkeleton',
+    defaultStyles: {
+      backgroundColor: 'currentColor',
+      display: 'block',
+      willChange: 'opacity',
+      animation: `${skeletonAnimation} .8s linear infinite alternate`,
+    },
+  },
+);
