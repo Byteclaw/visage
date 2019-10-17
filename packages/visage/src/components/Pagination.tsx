1810d0f83b246b009715896e3c1f44978da0db33
jurajhrib <ancientpedestrian@gmail.com>
2019-10-17 20:11:01 +0200

    add next page button

--- packages/visage/src/components/Pagination.tsx
+++ packages/visage/src/components/Pagination.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
+import { ExtractVisageComponentProps } from '@byteclaw/visage-core';
+import { Button } from './Button';
+import { NextPageIcon } from '../assets';
+import { SvgIcon } from './SvgIcon';
 
-export const NextPageButton = () => null;
+export const NextPageButton: typeof Button = forwardRef(
+  (
+    {
+      children = (
+        <SvgIcon
+          aria-hidden
+          focusable={false}
+          icon={NextPageIcon}
+          styles={{ width: '1em', height: '1em' }}
+        />
+      ),
+      ...props
+    }: ExtractVisageComponentProps<typeof Button>,
+    ref: any,
+  ) => (
+    <Button type="button" ref={ref} {...props}>
+      {children}
+    </Button>
+  ),
+) as any;
 
 export const PreviousPageButton = () => null;
 
