ecbf2cb6abc981d53b0464dea2f04895ad3ecf02
jurajhrib <ancientpedestrian@gmail.com>
2019-10-17 20:04:09 +0200

    add pagination docs

---
+++ packages/visage/src/components/Pagination.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const NextPageButton = () => null;
+
+export const PreviousPageButton = () => null;
+
+export const Pagination = ({ children }: any) => <div>{children}</div>;
