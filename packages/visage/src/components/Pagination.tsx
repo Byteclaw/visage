242e521400307f2121e3aa1dc2e1a7eed55ec7b9
jurajhrib <ancientpedestrian@gmail.com>
2019-10-17 21:43:11 +0200

    remove typeof types

--- packages/visage/src/components/Pagination.tsx
+++ packages/visage/src/components/Pagination.tsx
@@ -1,18 +1,68 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { ExtractVisageComponentProps } from '@byteclaw/visage-core';
+import { PreviousPageIcon, NextPageIcon } from '../assets';
 import { Button } from './Button';
+import { Flex } from './Flex';
+import { SvgIcon } from './SvgIcon';
 
-export function NextPageButton({
-  children,
-  ...props
-}: ExtractVisageComponentProps<typeof Button>) {
-  return (
-    <Button type="button" {...props}>
-      {children}
-    </Button>
-  );
+interface PaginationProps extends ExtractVisageComponentProps<typeof Flex> {
+  ariaLabel?: string;
 }
 
-export const PreviousPageButton = () => null;
+export const NextPageButton: typeof Button = forwardRef(
+  (
+    {
+      children = (
+        <SvgIcon
+          aria-hidden
+          focusable={false}
+          icon={NextPageIcon}
+          styles={{ width: '1em', height: '1em' }}
+        />
+      ),
+      ...props
+    }: any,
+    ref: any,
+  ) => (
+    <Button type="button" ref={ref} {...props}>
+      {children}
+    </Button>
+  ),
+) as any;
 
-export const Pagination = ({ children }: any) => <div>{children}</div>;
+export const PreviousPageButton: typeof Button = forwardRef(
+  (
+    {
+      children = (
+        <SvgIcon
+          aria-hidden
+          focusable={false}
+          icon={PreviousPageIcon}
+          styles={{ width: '1em', height: '1em' }}
+        />
+      ),
+      ...props
+    }: any,
+    ref: any,
+  ) => (
+    <Button type="button" ref={ref} {...props}>
+      {children}
+    </Button>
+  ),
+) as any;
+
+export const Pagination: typeof Flex = ({
+  ariaLabel = 'pagination',
+  children,
+  styles,
+  ...props
+}: PaginationProps) => (
+  <Flex
+    aria-label={ariaLabel}
+    as="nav"
+    styles={{ justifyContent: 'flex-start', ...styles }}
+    {...props}
+  >
+    {children}
+  </Flex>
+);
