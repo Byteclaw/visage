325ee090603fe9ece6464acf3b3bad41a50980da
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-11-30 14:02:23 +0100

    configure algolia indexing

--- packages/visage-docs/gatsby-config.js
+++ packages/visage-docs/gatsby-config.js
@@ -1,4 +1,5 @@
 const { resolve } = require('path');
+const algoliaQueries = require('./utils/algolia');
 
 module.exports = {
   siteMetadata: {
@@ -54,6 +55,15 @@ module.exports = {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-153604238-1',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-algolia',
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries: algoliaQueries,
+        chunkSize: 10000,
       },
     },
   ],
