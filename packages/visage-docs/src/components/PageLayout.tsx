52c21dd3b758a7737689bf486244a5ffffd49ee3
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-11-30 14:43:10 +0100

    add edit on GitHub link to pages, closes #208

--- packages/visage-docs/src/components/PageLayout.tsx
+++ packages/visage-docs/src/components/PageLayout.tsx
@@ -1,3 +1,4 @@
+import { Box, Link } from '@byteclaw/visage';
 import React, { ReactNode } from 'react';
 import { SEO } from './SEO';
 
@@ -12,13 +13,27 @@ interface PageLayoutProps {
   uri: string;
 }
 
-export function PageLayout({ children, pageContext, path }: PageLayoutProps) {
+export function PageLayout({
+  children,
+  pageContext,
+  path,
+  uri,
+}: PageLayoutProps) {
   const { frontmatter } = pageContext;
 
   return (
     <>
       <SEO {...frontmatter} pathname={path} />
       {children}
+      <Box>
+        <Link
+          href={`https://github.com/Byteclaw/visage/edit/master/packages/visage-docs/src/pages${uri}${
+            uri === '/' ? 'index.mdx' : '.mdx'
+          }`}
+        >
+          Edit on GitHub
+        </Link>
+      </Box>
     </>
   );
 }
