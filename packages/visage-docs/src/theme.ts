65115378aafd9f9518317bf342b5fb7a60631cb7
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-12-17 15:42:46 +0100

    use new theme in docs and add dark/light theme toggle

--- packages/visage-docs/src/theme.ts
+++ packages/visage-docs/src/theme.ts
@@ -1,49 +1,24 @@
 import color from 'color';
-import { ScaleValue } from '@byteclaw/visage-utils';
-import { createScaleTheme, ratios } from '@byteclaw/visage';
+import { createContext } from 'react';
+import { createScaleTheme, ratios, ColorPalette } from '@byteclaw/visage';
 
-function generateScale<TKey extends string = string>(
-  name: TKey,
-  forColor: string,
-  lightSteps: number = 0,
-  darkSteps: number = 0,
-): Record<TKey, ScaleValue<string>> {
-  const col = color(forColor);
-  const lghtns = col.lightness();
-  const colors: string[] = [];
+export const ThemeTogglerContext = createContext<{
+  isDark: boolean;
+  useDark(use: boolean): void;
+}>({} as any);
 
-  for (let i = lightSteps; i > 0; --i) {
-    colors.push(
-      col
-        .lightness(lghtns + (100 - lghtns) * ((1 / (lightSteps + 1)) * i))
-        .rgb()
-        .toString(),
-    );
-  }
+ThemeTogglerContext.displayName = 'ThemeToggler';
 
-  colors.push(col.hex().toString());
-
-  for (let i = 1; i < darkSteps + 1; ++i) {
-    colors.push(
-      col
-        .lightness(lghtns - lghtns * ((1 / (lightSteps + 1)) * i))
-        .rgb()
-        .toString(),
-    );
-  }
-
+function createColors(colors: ColorPalette): ColorPalette {
   return {
-    [name]: {
-      values: colors,
-      offset: lightSteps,
-    },
-    [`${name}Text`]: {
-      values: colors.map(bgColor =>
-        color(bgColor).isDark() ? 'white' : 'black',
-      ),
-      offset: lightSteps,
-    },
-  } as Record<TKey, ScaleValue<string>>;
+    ...colors,
+    bodyText: color(colors.lightShades).isDark() ? 'white' : 'black',
+    lightAccentText: color(colors.lightAccent).isDark() ? 'white' : 'black',
+    lightShadesText: color(colors.lightShades).isDark() ? 'white' : 'black',
+    darkAccentText: color(colors.darkAccent).isDark() ? 'white' : 'black',
+    darkShadesText: color(colors.darkShades).isDark() ? 'white' : 'black',
+    primaryText: color(colors.primary).isDark() ? 'white' : 'black',
+  };
 }
 
 export const theme = createScaleTheme({
@@ -51,18 +26,31 @@ export const theme = createScaleTheme({
   lineHeights: { values: [18, 20, 22, 24, 28, 32, 36, 40, 48, 56], offset: 2 },
   baselineGridSize: 8,
   fontScaleRatio: ratios.perfectFourth,
-  colors: {
-    body: 'white',
-    bodyText: 'black',
-    dangerBodyText: 'red',
-    ...generateScale<'danger' | 'dangerText'>('danger', 'red', 5, 5),
-    ...generateScale<'info' | 'infoText'>('info', '#0099ff', 5, 5),
-    ...generateScale<'neutral' | 'neutralText'>('neutral', '#ddd', 5, 5),
-    ...generateScale<'primary' | 'primaryText'>('primary', '#000', 6),
-    ...generateScale<'grey' | 'greyText'>('grey', '#999', 5, 5),
-    ...generateScale<'success' | 'successText'>('success', '#99cc33', 5, 5),
-    ...generateScale<'warning' | 'warningText'>('warning', '#ffcc00', 5, 5),
+  colors: createColors({
+    primary: '#E26F4A',
+    lightAccent: '#D3BF9C',
+    lightShades: '#F2F6E7',
+    darkAccent: '#D43E62',
+    darkShades: '#241828',
+  }),
+  fontFamilies: {
+    body: 'Open Sans,sans-serif',
+    heading: 'Nunito Sans,sans-serif',
   },
+});
+
+export const darkTheme = createScaleTheme({
+  fontSizes: { values: [10, 12, 14, 16, 20, 24, 28, 32, 40, 48], offset: 2 },
+  lineHeights: { values: [18, 20, 22, 24, 28, 32, 36, 40, 48, 56], offset: 2 },
+  baselineGridSize: 8,
+  fontScaleRatio: ratios.perfectFourth,
+  colors: createColors({
+    primary: '#E26F4A',
+    darkAccent: '#D3BF9C',
+    darkShades: '#F2F6E7',
+    lightAccent: '#D43E62',
+    lightShades: '#241828',
+  }),
   fontFamilies: {
     body: 'Open Sans,sans-serif',
     heading: 'Nunito Sans,sans-serif',
