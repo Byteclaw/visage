3bf72bfd0d7a2255ecbf9b321194f05769c08b41
jurajhrib <ancientpedestrian@gmail.com>
2019-12-18 23:18:07 +0100

    enhance docs layout

--- packages/visage-docs/src/components/Layout.tsx
+++ packages/visage-docs/src/components/Layout.tsx
@@ -1,12 +1,13 @@
 import {
   Column,
   Container,
+  Flex,
   Header,
   SvgIcon,
   Toggle,
   Text,
 } from '@byteclaw/visage';
-import React, { ReactNode } from 'react';
+import React, { Fragment, ReactNode } from 'react';
 // @ts-ignore
 import { ReactComponent as LogoSvg } from '../../static/logo.svg';
 import { ThemeTogglerContext } from '../theme';
@@ -20,34 +21,50 @@ interface Props {
 
 export function Layout({ children }: Props) {
   return (
-    <>
-      <Header styles={{ alignItems: 'center', pr: 1, py: 1 }}>
-        <SvgIcon icon={LogoSvg} styles={{ iconSize: 2 }} />{' '}
-        <Text styles={{ fontSize: 2, lineHeight: 2 }}>Visage</Text>
-        <ThemeTogglerContext.Consumer>
-          {value => (
-            <>
-              <Toggle
-                label="Use dark theme"
-                hiddenLabel
-                onChange={e => value.useDark(e.currentTarget.checked)}
-                checked={value.isDark}
-                styles={{ mx: 2 }}
-              />
-              <GeneratePaletteButton onSuccess={value.setColors} />
-            </>
-          )}
-        </ThemeTogglerContext.Consumer>
-        <Search />
+    <Fragment>
+      <Header styles={{ py: 1 }}>
+        <Container styles={{ px: 6, alignItems: 'center' }}>
+          <Flex styles={{ width: '20rem', flexShrink: 0, px: 3 }}>
+            <SvgIcon icon={LogoSvg} styles={{ iconSize: 2, mr: 0 }} />{' '}
+            <Text styles={{ fontSize: 2, lineHeight: 2, fontWeight: 'bold' }}>
+              Visage
+            </Text>
+          </Flex>
+          <Flex styles={{ flex: 1, pl: 6 }}>
+            <Search />
+            <ThemeTogglerContext.Consumer>
+              {value => (
+                <>
+                  <Toggle
+                    label="Use dark theme"
+                    hiddenLabel
+                    onChange={e => value.useDark(e.currentTarget.checked)}
+                    checked={value.isDark}
+                    styles={{ mx: 2 }}
+                  />
+                  <GeneratePaletteButton onSuccess={value.setColors} />
+                </>
+              )}
+            </ThemeTogglerContext.Consumer>
+          </Flex>
+        </Container>
       </Header>
-      <Container>
-        <Column as="nav" styles={{ width: '16rem', flexShrink: 0 }}>
+      <Container styles={{ px: 6 }}>
+        <Column as="nav" styles={{ width: '20rem', flexShrink: 0, px: 2 }}>
           <Sidebar />
         </Column>
-        <Column styles={{ px: 2, width: '100%', '& > div': { width: '100%' } }}>
+        <Column
+          styles={{
+            pl: 6,
+            pt: 4,
+            pb: 6,
+            width: '100%',
+            '& > div': { width: '100%' },
+          }}
+        >
           {children}
         </Column>
       </Container>
-    </>
+    </Fragment>
   );
 }
