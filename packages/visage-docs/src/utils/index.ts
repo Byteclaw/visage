cb56ff06b37727bc9db84eb829603feac4104305
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-12-20 12:38:52 +0100

    generate color scale for docs theme's neutral color

--- packages/visage-docs/src/utils/index.ts
+++ packages/visage-docs/src/utils/index.ts
@@ -1,5 +1,2 @@
-import createSlug from 'slugify';
-
-export function slugify(text: string): string {
-  return createSlug(text, { lower: true });
-}
+export * from './generateColorScale';
+export * from './slugify';
