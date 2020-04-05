24d0ff722275fac8046be232b01743b978477824
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-04-05 11:06:48 +0200

    build: validate scopes by commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  'header-max-length': 86,
+  'scope-enum': [
+    'visage',
+    'visage/babel',
+    'visage/core',
+    'visage/docs',
+    'visage/themes',
+    'visage/utils',
+  ],
+};
