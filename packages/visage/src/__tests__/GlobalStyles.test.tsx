91db2fdb0a7c92ce49ca7d285f007b9bf3bbd442
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-04-05 14:26:49 +0200

    build: replace microbundle with tsdx

--- packages/visage/src/__tests__/GlobalStyles.test.tsx
+++ packages/visage/src/__tests__/GlobalStyles.test.tsx
@@ -13,33 +13,30 @@ describe('GlobalStyles', () => {
 
     expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
     expect(getComputedStyle(document.body)).toMatchInlineSnapshot(`
-CSSStyleDeclaration {
-  "0": "display",
-  "1": "margin",
-  "2": "font-family",
-  "3": "font-size",
-  "4": "visibility",
-  "_importants": Object {
-    "display": "",
-    "font-family": "",
-    "font-size": "",
-    "margin": "",
-    "visibility": undefined,
-  },
-  "_length": 5,
-  "_onChange": [Function],
-  "_values": Object {
-    "display": "block",
-    "font-family": "body-font",
-    "font-size": "26px",
-    "margin": "8px",
-    "margin-bottom": "8px",
-    "margin-left": "8px",
-    "margin-right": "8px",
-    "margin-top": "8px",
-    "visibility": "visible",
-  },
-}
-`);
+      CSSStyleDeclaration {
+        "0": "display",
+        "1": "margin",
+        "2": "font-family",
+        "3": "font-size",
+        "_importants": Object {
+          "display": "",
+          "font-family": "",
+          "font-size": "",
+          "margin": "",
+        },
+        "_length": 4,
+        "_onChange": [Function],
+        "_values": Object {
+          "display": "block",
+          "font-family": "body-font",
+          "font-size": "26px",
+          "margin": "8px",
+          "margin-bottom": "8px",
+          "margin-left": "8px",
+          "margin-right": "8px",
+          "margin-top": "8px",
+        },
+      }
+    `);
   });
 });
