e0036ba4860a7eda9c1b3eb657076d1df5129143
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-04-19 17:03:30 +0200

    fix(visage/themes): fix box shadow color parsing

---
+++ packages/visage-themes/box-shadow-grammar.pegjs
@@ -0,0 +1,23 @@
+start = "none" / "initial" / "inherit" / "unset" / bss:boxShadow+ { return bss; }
+
+boxShadow = boxShadow4 / boxShadow3 / boxShadow2
+boxShadow4 = _ "inset"? _ offset _ offset _ offset _ offset _ col:color { return { color: col }; }
+boxShadow3 = _ "inset"? _ offset _ offset _ offset _ col:color { return { color: col }; }
+boxShadow2 = _ "inset"? _ offset _ offset _ col:color { return { color: col }; }
+
+color = col:$(colorMod / colorFn / colorHex / colorWord)
+colorFn = ("rgb" / "hsl") "a"? "(" _ nop _ nop _ nop _ n? _ ")"
+colorMod = "color(" _ color _ adj* _")"
+colorHex = "#" ((h h h h h h) / (h h h))
+colorWord = $([a-zA-Z]+ ("." "-"? [0-9]+)?)
+offset = (("-"? n) / ("."[0-9]+)) [a-zA-Z]*
+adj = _ (adjIf / adjIfCol / adjNum) _
+adjNum = [a-z]+ "(" _ ("+" / "-")? _ nop? _ ")"
+adjIf = "if(" _ [a-zA-Z]+ _ (colorMod / colorFn / colorHex / colorWord) _ (colorMod / colorFn / colorHex / colorWord)? _ ")"
+adjIfCol = ("ifDark" / "ifLight" / "blenda" / "blend") "(" _ (colorMod / colorFn / colorHex / colorWord ) _ np? _ ")"
+h = [0-9a-fA-F]+
+n = [0-9]+ ("." [0-9]+)?
+np = n "%"
+nop = np / n
+
+_ "whitespace" = [ \t\n\r,]*
---
+++ packages/visage-themes/src/__tests__/boxShadowParser.test.ts
@@ -0,0 +1,120 @@
+import { transformSync } from '@babel/core';
+import * as fs from 'fs';
+import * as path from 'path';
+import { generate } from 'pegjs';
+import tspegjs from 'ts-pegjs';
+
+describe('box shadow parser', () => {
+  // eslint-disable-next-line no-eval
+  const parse = eval(
+    transformSync(
+      generate(
+        fs.readFileSync(
+          path.resolve(__dirname, '../../box-shadow-grammar.pegjs'),
+          {
+            encoding: 'utf8',
+          },
+        ),
+        {
+          output: 'source',
+          plugins: [tspegjs],
+        },
+      ),
+      {
+        filename: 'parser.ts',
+      },
+    ).code,
+  );
+
+  it('parses simple box shadow', () => {
+    expect(parse('unset')).toEqual('unset');
+    expect(parse('initial')).toEqual('initial');
+    expect(parse('unset')).toEqual('unset');
+    expect(parse('none')).toEqual('none');
+    expect(parse('0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset 0px 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('0px 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset 0px 0.0em red')).toEqual([{ color: 'red' }]);
+    expect(parse('0px 0.0em red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset -0px .0em red')).toEqual([{ color: 'red' }]);
+    expect(parse('-0px .0em red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset -0px .0em red.1')).toEqual([{ color: 'red.1' }]);
+    expect(parse('-0px .0em red.1')).toEqual([{ color: 'red.1' }]);
+    expect(parse('inset -0px .0em red.-1')).toEqual([{ color: 'red.-1' }]);
+    expect(parse('-0px .0em red.-1')).toEqual([{ color: 'red.-1' }]);
+    expect(parse('inset -0px .0em color(red alpha(0.2))')).toEqual([
+      { color: 'color(red alpha(0.2))' },
+    ]);
+    expect(parse('-0px .0em color(red alpha(0.2))')).toEqual([
+      { color: 'color(red alpha(0.2))' },
+    ]);
+
+    expect(parse('0 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset 0 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset 0px 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('0px 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset 0px 0.0em 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('0px 0.0em 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset -0px .0em 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('-0px .0em 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset -0px .0em 0 red.1')).toEqual([{ color: 'red.1' }]);
+    expect(parse('-0px .0em 0 red.1')).toEqual([{ color: 'red.1' }]);
+    expect(parse('inset -0px .0em 0 red.-1')).toEqual([{ color: 'red.-1' }]);
+    expect(parse('-0px .0em 0 red.-1')).toEqual([{ color: 'red.-1' }]);
+    expect(parse('inset -0px .0em 0 color(red alpha(0.2))')).toEqual([
+      { color: 'color(red alpha(0.2))' },
+    ]);
+    expect(parse('-0px .0em 0 color(red alpha(0.2))')).toEqual([
+      { color: 'color(red alpha(0.2))' },
+    ]);
+
+    expect(parse('0 0 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset 0 0 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset 0px 0 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('0px 0 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset 0px 0.0em 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('0px 0.0em 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset -0px .0em 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('-0px .0em 0 0 red')).toEqual([{ color: 'red' }]);
+    expect(parse('inset -0px .0em 0 0 red.1')).toEqual([{ color: 'red.1' }]);
+    expect(parse('-0px .0em 0 0 red.1')).toEqual([{ color: 'red.1' }]);
+    expect(parse('inset -0px .0em 0 0 red.-1')).toEqual([{ color: 'red.-1' }]);
+    expect(parse('-0px .0em 0 0 red.-1')).toEqual([{ color: 'red.-1' }]);
+    expect(parse('inset -0px .0em 0 0 color(red alpha(0.2))')).toEqual([
+      { color: 'color(red alpha(0.2))' },
+    ]);
+    expect(parse('-0px .0em 0 0 color(red alpha(0.2))')).toEqual([
+      { color: 'color(red alpha(0.2))' },
+    ]);
+    expect(parse('inset -0px .0em 0 0 color(red contrast())')).toEqual([
+      { color: 'color(red contrast())' },
+    ]);
+    expect(parse('-0px .0em 0 0 color(red contrast(20%))')).toEqual([
+      { color: 'color(red contrast(20%))' },
+    ]);
+    expect(parse('-0px .0em 0 0 color(red ifDark(white))')).toEqual([
+      { color: 'color(red ifDark(white))' },
+    ]);
+    expect(parse('-0px .0em 0 0 color(red if(isDark white))')).toEqual([
+      { color: 'color(red if(isDark white))' },
+    ]);
+    expect(parse('-0px .0em 0 0 color(red if(isDark white black))')).toEqual([
+      { color: 'color(red if(isDark white black))' },
+    ]);
+    expect(parse('-0px .0em 0 0 color(red blenda(white 10%))')).toEqual([
+      { color: 'color(red blenda(white 10%))' },
+    ]);
+  });
+
+  it('parses multiple box shadow', () => {
+    expect(parse('0 0 red, 0 0 blue, inset 0px -1em black')).toEqual([
+      { color: 'red' },
+      { color: 'blue' },
+      { color: 'black' },
+    ]);
+    expect(
+      parse('0 0 0 4px rgba(255, 255, 255, 0.4), 0 0 0 4px accent'),
+    ).toEqual([{ color: 'rgba(255, 255, 255, 0.4)' }, { color: 'accent' }]);
+  });
+});
