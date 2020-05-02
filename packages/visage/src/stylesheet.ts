6b681186bbf5f76becf88f56ac2350d882daf502
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-05-02 14:09:50 +0200

    feat(visage): relax fontWeight type

--- packages/visage/src/stylesheet.ts
+++ packages/visage/src/stylesheet.ts
@@ -1,7 +1,8 @@
 import { StyleSheet } from '@byteclaw/visage-core';
 
 declare global {
-  export interface VisageStylingProperties extends React.CSSProperties {
+  export interface VisageStylingProperties
+    extends Omit<React.CSSProperties, 'fontWeight'> {
     /**
      * Post style sheet that overrides current style sheet
      */
@@ -23,21 +24,62 @@ declare global {
      * Use same value as for line height to set a width of an element
      */
     linedWidth?: number | string;
+    fontWeight?: React.CSSProperties['fontWeight'] | string;
+    /**
+     * Shorthand for margin
+     */
     m?: string | number;
+    /**
+     * Shorthand for margin-top, margin-bottom
+     */
     my?: string | number;
+    /**
+     * Shorthand for margin-left, margin-right
+     */
     mx?: string | number;
+    /**
+     * Shorthand for margin-bottom
+     */
     mb?: string | number;
+    /**
+     * Shorthand for margin-left
+     */
     ml?: string | number;
+    /**
+     * Shorthand for margin-right
+     */
     mr?: string | number;
+    /**
+     * Shorthand for margin-top
+     */
     mt?: string | number;
+    /**
+     * Shorthand for padding
+     */
     p?: string | number;
+    /**
+     * Shorthand for padding-top, padding-bottom
+     */
     py?: string | number;
+    /**
+     * Shorthand for padding-left, padding-right
+     */
     px?: string | number;
+    /**
+     * Shorthand for padding-bottom
+     */
     pb?: string | number;
+    /**
+     * Shorthand for padding-left
+     */
     pl?: string | number;
-    plOffset?: string | number;
-    prOffset?: string | number;
+    /**
+     * Shorthand for padding-right
+     */
     pr?: string | number;
+    /**
+     * Shorthand for padding-top
+     */
     pt?: string | number;
   }
 
