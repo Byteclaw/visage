a4ddec3a0c9bdc354a11da86f3cb387dbad8d3f9
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-05-07 15:08:47 +0200

    feat(visage): add inverted variant of Message component

--- packages/visage/src/components/Message.tsx
+++ packages/visage/src/components/Message.tsx
@@ -4,97 +4,188 @@ import React, {
   useCallback,
   MouseEventHandler,
   ReactElement,
+  forwardRef,
+  Ref,
 } from 'react';
-import { ExtractVisageComponentProps } from '@byteclaw/visage-core';
+import {
+  ExtractVisageComponentProps,
+  VisageComponent,
+  markAsVisageComponent,
+} from '@byteclaw/visage-core';
 import { createComponent } from '../core';
-import { variant } from '../variants';
+import {
+  booleanVariant,
+  booleanVariantStyles,
+  variant,
+  variantStyles,
+} from '../variants';
 import { CloseButton } from './CloseButton';
-
-const variantStyles = {
-  danger: {
-    backgroundColor: 'danger',
-    color: 'dangerText',
-  },
-  info: {
-    backgroundColor: 'info',
-    color: 'infoText',
-  },
-  success: {
-    backgroundColor: 'success',
-    color: 'successText',
-  },
-  warning: {
-    backgroundColor: 'warning',
-    color: 'warningText',
-  },
-  default: {
-    backgroundColor: 'neutral',
-    color: 'neutralText',
-  },
-};
+import { createControlFocusShadow, createSurfaceFocusShadow } from './shared';
 
 const MessageBase = createComponent('div', {
   displayName: 'Message',
-  styles: props => ({
+  styles: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     fontSize: 0,
     p: 1,
-    ...(variantStyles[props.variant || 'default'] || variantStyles.default),
-  }),
+    outline: 'none',
+    position: 'relative',
+    '&:focus': {
+      zIndex: 1,
+    },
+    ...variantStyles('status', {
+      danger: booleanVariantStyles('inverted', {
+        on: {
+          color: 'danger',
+          '&:focus': {
+            boxShadow: createSurfaceFocusShadow('danger'),
+          },
+        },
+        off: {
+          backgroundColor: 'danger',
+          color: 'dangerText',
+          '&:focus': {
+            boxShadow: createControlFocusShadow('danger'),
+          },
+        },
+      }),
+      info: booleanVariantStyles('inverted', {
+        on: {
+          color: 'info',
+          '&:focus': {
+            boxShadow: createSurfaceFocusShadow('info'),
+          },
+        },
+        off: {
+          backgroundColor: 'info',
+          color: 'infoText',
+          '&:focus': {
+            boxShadow: createControlFocusShadow('info'),
+          },
+        },
+      }),
+      success: booleanVariantStyles('inverted', {
+        on: {
+          color: 'success',
+          '&:focus': {
+            boxShadow: createSurfaceFocusShadow('success'),
+          },
+        },
+        off: {
+          backgroundColor: 'success',
+          color: 'successText',
+          '&:focus': {
+            boxShadow: createControlFocusShadow('success'),
+          },
+        },
+      }),
+      warning: booleanVariantStyles('inverted', {
+        on: {
+          color: 'warning',
+          '&:focus': {
+            boxShadow: createSurfaceFocusShadow('warning'),
+          },
+        },
+        off: {
+          backgroundColor: 'warning',
+          color: 'warningText',
+          '&:focus': {
+            boxShadow: createControlFocusShadow('warning'),
+          },
+        },
+      }),
+      default: booleanVariantStyles('inverted', {
+        on: {
+          color: 'shadesText',
+          '&:focus': {
+            boxShadow: createSurfaceFocusShadow(),
+          },
+        },
+        off: {
+          backgroundColor: 'neutral',
+          color: 'neutralText',
+          '&:focus': {
+            boxShadow: createControlFocusShadow('neutral'),
+          },
+        },
+      }),
+    }),
+  },
   variants: [
-    variant('variant', true, [
+    variant('status', true, [
       'danger',
       'info',
       'success',
       'warning',
       'default',
     ] as const),
+    booleanVariant('inverted', true),
   ],
 });
 
-const MessageDeleteButton = createComponent(CloseButton, {
-  displayName: 'MessageDeleteButton',
+const MessageDismissButton = createComponent(CloseButton, {
+  displayName: 'MessageDismissButton',
   styles: {
     ml: 1,
     p: 0.5,
   },
 });
 
-const defaultChipDeleteRenderer = (props: {
+const defaultMessageDismissButton = (props: {
   onClick: MouseEventHandler<any>;
-}) => <MessageDeleteButton {...props} />;
+}) => <MessageDismissButton {...props} />;
 
 interface MessageProps extends ExtractVisageComponentProps<typeof MessageBase> {
   /**
    * Makes message dismissable
    *
-   * Called when user click on delete element
+   * Called when user click on dismiss element
    */
-  onDelete?: (e: KeyboardEvent<any> | MouseEvent<any>) => void;
-  renderDeleter?: (props: { onClick: MouseEventHandler<any> }) => ReactElement;
+  onDismiss?: (e: KeyboardEvent<any> | MouseEvent<any>) => void;
+  /**
+   * Render function that can be used to render custom dismiss button
+   */
+  renderDismiss?: (props: { onClick: MouseEventHandler<any> }) => ReactElement;
 }
 
-export function Message({
-  children,
-  onDelete,
-  renderDeleter = defaultChipDeleteRenderer,
-  ...restProps
-}: MessageProps) {
-  const onDeleteClick = useCallback(
-    (e: MouseEvent<any>) => {
-      if (onDelete) {
-        onDelete(e);
-      }
+export const Message: VisageComponent<MessageProps> = markAsVisageComponent(
+  forwardRef(
+    (
+      {
+        children,
+        onDismiss,
+        renderDismiss = defaultMessageDismissButton,
+        status,
+        ...restProps
+      }: MessageProps,
+      ref: Ref<any>,
+    ) => {
+      const onDismissClick = useCallback(
+        (e: MouseEvent<any>) => {
+          if (onDismiss) {
+            onDismiss(e);
+          }
+        },
+        [onDismiss],
+      );
+
+      return (
+        <MessageBase
+          aria-live="polite"
+          role={
+            status === 'danger' || status === 'warning' ? 'alert' : 'status'
+          }
+          status={status}
+          tabIndex={0}
+          ref={ref}
+          {...restProps}
+        >
+          {children}
+          {onDismiss ? renderDismiss({ onClick: onDismissClick }) : null}
+        </MessageBase>
+      );
     },
-    [onDelete],
-  );
-
-  return (
-    <MessageBase {...restProps}>
-      {children}
-      {onDelete ? renderDeleter({ onClick: onDeleteClick }) : null}
-    </MessageBase>
-  );
-}
+  ),
+) as any;
