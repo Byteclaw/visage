95f8c20a4e0e79daccdb951c28821b29f5d1c24e
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-05-20 19:16:25 +0200

    feat(visage/core): allow to pass custom class names

--- packages/visage/src/__tests__/core.test.tsx
+++ packages/visage/src/__tests__/core.test.tsx
@@ -5,6 +5,55 @@ import { DesignSystem } from '../DesignSystem';
 import { theme } from './TestDesignSystem';
 
 describe('core', () => {
+  it('allows to pass custom className', () => {
+    const A = createComponent('span', {
+      defaultStyles: {
+        color: 'black',
+        margin: 1,
+      },
+    });
+
+    const { asFragment } = render(
+      <DesignSystem theme={theme}>
+        <A className="test-class-name" />
+      </DesignSystem>,
+    );
+
+    expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        .emotion-0 {
+        -webkit-box-pack: center;
+        -webkit-justify-content: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: 600;
+      }
+
+      <div
+          aria-live="polite"
+          class="emotion-0"
+          data-toastmanager="true"
+        />
+        .emotion-0 {
+        color: rgb(0,0,0);
+        margin: 8px;
+      }
+
+      <span
+          class="emotion-0 test-class-name"
+        />
+      </DocumentFragment>
+    `);
+  });
+
   describe('extending', () => {
     it('extends visage component correctly', () => {
       const A = createComponent('span', {
