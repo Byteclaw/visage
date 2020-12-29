8e426917b633a4a4cd753c74b5edd49631502df9
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-12-29 11:14:42 +0100

    fix: pass ref to file input

--- packages/visage/src/components/FileInput.tsx
+++ packages/visage/src/components/FileInput.tsx
@@ -1,4 +1,7 @@
-import { ExtractVisageComponentProps } from '@byteclaw/visage-core';
+import {
+  ExtractVisageComponentProps,
+  markAsVisageComponent,
+} from '@byteclaw/visage-core';
 import React, {
   DragEventHandler,
   ChangeEventHandler,
@@ -17,6 +20,7 @@ import {
   visuallyHiddenStyles,
   createSurfaceFocusShadow,
 } from './shared';
+import { useCombinedRef } from '../hooks';
 
 const BaseFileInput = createComponent('input', {
   displayName: 'FileInputControl',
@@ -105,132 +109,143 @@ interface FileInputProps extends BaseFileInputProps {
   onChange?: (files: File[]) => void;
 }
 
-export function FileInput({
-  accept,
-  baseProps,
-  controlProps,
-  disabled,
-  invalid,
-  label = defaultLabelFormatter,
-  multiple,
-  onChange,
-  readOnly,
-  placeholder = 'Choose a file',
-  ...restProps
-}: FileInputProps) {
-  const [dragging, setDragging] = useState(false);
-  const [files, setFiles] = useState<File[]>([]);
-  const previousFiles = useRef(files);
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  // const onDrag: DragEventHandler<HTMLDivElement> = useCallback(() => {}, []);
-  const onDragEnter: DragEventHandler<HTMLDivElement> = useCallback(e => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragging(true);
-  }, []);
-  const onDragOver: DragEventHandler<HTMLDivElement> = useCallback(e => {
-    e.preventDefault();
-    e.stopPropagation();
-    e.dataTransfer.dropEffect = 'copy';
-  }, []);
-  const onDragLeave: DragEventHandler<HTMLDivElement> = useCallback(e => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragging(false);
-  }, []);
-  const onDrop: DragEventHandler<HTMLDivElement> = useCallback(
-    e => {
+export const FileInput = markAsVisageComponent(
+  React.forwardRef(function FileInput(
+    {
+      accept,
+      baseProps,
+      controlProps,
+      disabled,
+      invalid,
+      label = defaultLabelFormatter,
+      multiple,
+      onChange,
+      readOnly,
+      placeholder = 'Choose a file',
+      ...restProps
+    }: FileInputProps,
+    ref: React.Ref<HTMLInputElement>,
+  ) {
+    const [dragging, setDragging] = useState(false);
+    const [files, setFiles] = useState<File[]>([]);
+    const previousFiles = useRef(files);
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const inputRefCallback = useCombinedRef(inputRef, ref);
+    // const onDrag: DragEventHandler<HTMLDivElement> = useCallback(() => {}, []);
+    const onDragEnter: DragEventHandler<HTMLDivElement> = useCallback(e => {
+      e.preventDefault();
+      e.stopPropagation();
+      setDragging(true);
+    }, []);
+    const onDragOver: DragEventHandler<HTMLDivElement> = useCallback(e => {
+      e.preventDefault();
+      e.stopPropagation();
+      e.dataTransfer.dropEffect = 'copy';
+    }, []);
+    const onDragLeave: DragEventHandler<HTMLDivElement> = useCallback(e => {
       e.preventDefault();
       e.stopPropagation();
       setDragging(false);
-
-      // if accept is filled, check if files are of the given types
-      const acceptedMediaTypes: string[] = accept ? accept.split(',') : ['*/*'];
-
-      const regex = new RegExp(
-        `^(${acceptedMediaTypes
-          .map(mediaType =>
-            mediaType.replace(/\//g, '\\/').replace(/\*/g, '[^\\/]+'),
-          )
-          .join('|')})$`,
-      );
-
-      // check if all files are accepted
-      if (Array.from(e.dataTransfer.files).every(f => regex.test(f.type))) {
-        setFiles(Array.from(e.dataTransfer.files));
-      }
-
-      e.dataTransfer.clearData();
-    },
-    [accept],
-  );
-  const onClick: MouseEventHandler = useCallback(() => {
-    if (inputRef.current) {
-      // delegate click to hidden file input
-      // if input is read only, just focus
-      if (readOnly) {
-        inputRef.current.focus();
-      } else {
-        inputRef.current.click();
-      }
-    }
-  }, [readOnly]);
-  const onInputChange: ChangeEventHandler<HTMLInputElement> = useCallback(e => {
-    if (e.currentTarget.files && e.currentTarget.files.length > 0) {
-      setFiles(Array.from(e.currentTarget.files));
-    }
-  }, []);
-  // disables keyboard interaction if input is readonly
-  const onInputKeyDown: KeyboardEventHandler<HTMLInputElement> = useCallback(
-    e => {
-      if (readOnly && (e.key === 'Enter' || e.key === ' ')) {
+    }, []);
+    const onDrop: DragEventHandler<HTMLDivElement> = useCallback(
+      e => {
         e.preventDefault();
+        e.stopPropagation();
+        setDragging(false);
+
+        // if accept is filled, check if files are of the given types
+        const acceptedMediaTypes: string[] = accept
+          ? accept.split(',')
+          : ['*/*'];
+
+        const regex = new RegExp(
+          `^(${acceptedMediaTypes
+            .map(mediaType =>
+              mediaType.replace(/\//g, '\\/').replace(/\*/g, '[^\\/]+'),
+            )
+            .join('|')})$`,
+        );
+
+        // check if all files are accepted
+        if (Array.from(e.dataTransfer.files).every(f => regex.test(f.type))) {
+          setFiles(Array.from(e.dataTransfer.files));
+        }
+
+        e.dataTransfer.clearData();
+      },
+      [accept],
+    );
+    const onClick: MouseEventHandler = useCallback(() => {
+      if (inputRef.current) {
+        // delegate click to hidden file input
+        // if input is read only, just focus
+        if (readOnly) {
+          inputRef.current.focus();
+        } else {
+          inputRef.current.click();
+        }
       }
-    },
-    [readOnly],
-  );
+    }, [readOnly]);
+    const onInputChange: ChangeEventHandler<HTMLInputElement> = useCallback(
+      e => {
+        if (e.currentTarget.files && e.currentTarget.files.length > 0) {
+          setFiles(Array.from(e.currentTarget.files));
+        }
+      },
+      [],
+    );
+    // disables keyboard interaction if input is readonly
+    const onInputKeyDown: KeyboardEventHandler<HTMLInputElement> = useCallback(
+      e => {
+        if (readOnly && (e.key === 'Enter' || e.key === ' ')) {
+          e.preventDefault();
+        }
+      },
+      [readOnly],
+    );
 
-  if (previousFiles.current !== files) {
-    previousFiles.current = files;
+    if (previousFiles.current !== files) {
+      previousFiles.current = files;
 
-    if (onChange) {
-      onChange(files);
+      if (onChange) {
+        onChange(files);
+      }
     }
-  }
 
-  const generatedLabel =
-    typeof label === 'function' ? label(files, placeholder) : label;
+    const generatedLabel =
+      typeof label === 'function' ? label(files, placeholder) : label;
 
-  return (
-    <FileInputBase {...baseProps}>
-      <BaseFileInput
-        {...restProps}
-        accept={accept}
-        aria-invalid={invalid}
-        aria-readonly={readOnly}
-        disabled={disabled}
-        multiple={multiple}
-        onKeyDown={onInputKeyDown}
-        onChange={onInputChange}
-        placeholder={placeholder}
-        ref={inputRef}
-        readOnly={readOnly}
-        type="file"
-      />
-      <FileInputControl
-        {...controlProps}
-        data-draggedover={dragging}
-        data-readonly={readOnly}
-        disabled={disabled}
-        invalid={invalid}
-        onClick={onClick}
-        onDragEnter={!readOnly ? onDragEnter : undefined}
-        onDragOver={!readOnly ? onDragOver : undefined}
-        onDragLeave={!readOnly ? onDragLeave : undefined}
-        onDrop={!readOnly ? onDrop : undefined}
-      >
-        {files.length === 0 ? placeholder : generatedLabel}
-      </FileInputControl>
-    </FileInputBase>
-  );
-}
+    return (
+      <FileInputBase {...baseProps}>
+        <BaseFileInput
+          {...restProps}
+          accept={accept}
+          aria-invalid={invalid}
+          aria-readonly={readOnly}
+          disabled={disabled}
+          multiple={multiple}
+          onKeyDown={onInputKeyDown}
+          onChange={onInputChange}
+          placeholder={placeholder}
+          ref={inputRefCallback}
+          readOnly={readOnly}
+          type="file"
+        />
+        <FileInputControl
+          {...controlProps}
+          data-draggedover={dragging}
+          data-readonly={readOnly}
+          disabled={disabled}
+          invalid={invalid}
+          onClick={onClick}
+          onDragEnter={!readOnly ? onDragEnter : undefined}
+          onDragOver={!readOnly ? onDragOver : undefined}
+          onDragLeave={!readOnly ? onDragLeave : undefined}
+          onDrop={!readOnly ? onDrop : undefined}
+        >
+          {files.length === 0 ? placeholder : generatedLabel}
+        </FileInputControl>
+      </FileInputBase>
+    );
+  }),
+);
