55a202ca46f70beac403cfeb2c4d0885bdd964f5
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-12-30 11:54:28 +0100

    feat: resolve color functions in theme colors

--- packages/visage-core/src/theme/__tests__/createTheme.test.ts
+++ packages/visage-core/src/theme/__tests__/createTheme.test.ts
@@ -44,4 +44,92 @@ describe('createTheme', () => {
       expect(themeKeyResolver).toHaveBeenCalledTimes(1);
     });
   });
+
+  describe('color resolution', () => {
+    it('allows to specify colors in theme using color function', () => {
+      const theme = createTheme({
+        theme: {
+          colors: {
+            primary: 'black',
+            primaryText: 'color(primary, contrast())',
+            cycle: 'color(cycle, contrast())',
+            indirectCycle: 'color(cycle, contrast())',
+            scale: {
+              offset: 1,
+              values: ['black', 'red', 'blue'],
+            },
+            scaleWithColor: {
+              values: [
+                'color(scale.-1, tint(10%))',
+                'color(scale, tint(10%))',
+                'color(scale.1, tint(10%))',
+              ],
+              offset: 1,
+            },
+            scaleWithCycle: {
+              values: ['cycle'],
+              offset: 0,
+            },
+          },
+          fontFamily: {},
+        },
+      });
+      const cache = new LRUCache<string, StylerResult>();
+
+      expect(
+        theme.resolve('color', 'primary', {
+          ...theme,
+          breakpoint: 0,
+          stylerCache: cache,
+        }),
+      ).toBe('rgb(0, 0, 0)');
+
+      expect(
+        theme.resolve('color', 'primaryText', {
+          ...theme,
+          breakpoint: 0,
+          stylerCache: cache,
+        }),
+      ).toBe('rgb(255, 255, 255)');
+
+      expect(
+        theme.resolve('color', 'cycle', {
+          ...theme,
+          breakpoint: 0,
+          stylerCache: cache,
+        }),
+      ).toEqual('color(cycle, contrast())');
+      expect(
+        theme.resolve('color', 'indirectCycle', {
+          ...theme,
+          breakpoint: 0,
+          stylerCache: cache,
+        }),
+      ).toEqual('color(cycle, contrast())');
+
+      expect(
+        theme.resolve('color', 'scale.1', {
+          ...theme,
+          breakpoint: 0,
+          stylerCache: cache,
+        }),
+      ).toBe('rgb(0, 0, 255)');
+
+      expect(
+        theme.resolve('color', 'scaleWithColor.1', {
+          ...theme,
+          breakpoint: 0,
+          stylerCache: cache,
+        }),
+      ).toBe('rgb(26, 26, 255)');
+
+      expect(
+        theme.resolve('color', 'scaleWithCycle.0', {
+          ...theme,
+          breakpoint: 0,
+          stylerCache: cache,
+        }),
+      ).toEqual('color(cycle, contrast())');
+    });
+  });
 });
